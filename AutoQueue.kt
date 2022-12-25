8c1bd4fbfc46e79a2a85ad0286d4ebac420813db
methylbenzene <99178854+m3ma110c@users.noreply.github.com>
2022-12-25 12:20:33 +0800

    :trollface:

---
+++ AutoQueue.kt
@@ -0,0 +1,62 @@
+package me.luna.trollhack.module.modules.misc
+
+import me.luna.trollhack.event.events.ConnectionEvent
+import me.luna.trollhack.event.events.PacketEvent
+import me.luna.trollhack.event.listener
+import me.luna.trollhack.module.Category
+import me.luna.trollhack.module.Module
+import me.luna.trollhack.util.text.MessageDetection
+import me.luna.trollhack.util.text.MessageSendUtils.sendServerMessage
+import net.minecraft.network.play.server.SPacketChat
+
+internal object AutoQueue : Module(
+    name = "AutoQueue",
+    description = "Automatically answer questions at 2b2t.xin queue",
+    category = Category.MISC
+) {
+    private val questions: HashMap<String,String> = hashMapOf(
+        "本服务" to "2020",
+        "苦力怕" to "爬行者",
+        "钻石的" to "264",
+        "大箱子" to "54",
+        "小箱子" to "27",
+        "羊驼会" to "不会",
+        "南瓜的" to "不需要",
+        "无限水" to "3",
+        "猪被闪" to "僵尸猪人",
+        "红石火" to "15",
+        "定位末" to "0",
+        "凋零死" to "下界之星",
+        "挖掘速" to "金镐"
+    )
+
+    private var is2B2T = false
+
+    init {
+        listener<ConnectionEvent.Connect>{
+            is2B2T = mc.currentServerData?.serverIP?.contains("2b2t.xin") == true
+        }
+
+        onEnable {
+            is2B2T = mc.currentServerData?.serverIP?.contains("2b2t.xin") == true
+        }
+
+        listener<PacketEvent.Receive> {
+            if(is2B2T){
+                if (it.packet !is SPacketChat || MessageDetection.Direct.RECEIVE detect it.packet.chatComponent.unformattedText) return@listener
+                if(it.packet.chatComponent.unformattedText.contains("A.")){
+                    var i:Int
+                    for ((question, answer) in questions) {
+                        if (it.packet.chatComponent.unformattedText.contains(question)) {
+                            i = it.packet.chatComponent.unformattedText.indexOf(answer, 0, false)
+                            if(i > 2){
+                                sendServerMessage(String.format("%s",it.packet.chatComponent.unformattedText.substring(i - 2, i - 1)))
+                            }
+                            break
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
