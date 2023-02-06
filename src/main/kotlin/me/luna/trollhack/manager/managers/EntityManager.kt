83cbf719e84e7e62e60193f3a7d700ff41b73320
Luna <62033805+Luna5ama@users.noreply.github.com>
2023-02-06 11:09:35 -0500

    EntityManager look up by id

--- src/main/kotlin/me/luna/trollhack/manager/managers/EntityManager.kt
+++ src/main/kotlin/me/luna/trollhack/manager/managers/EntityManager.kt
@@ -1,5 +1,8 @@
 package me.luna.trollhack.manager.managers
 
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap
+import it.unimi.dsi.fastutil.ints.Int2ObjectMaps
+import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap
 import me.luna.trollhack.event.events.ConnectionEvent
 import me.luna.trollhack.event.events.TickEvent
 import me.luna.trollhack.event.events.WorldEvent
@@ -24,33 +27,59 @@ object EntityManager : Manager() {
     val players: List<EntityPlayer>
         get() = players0
 
+    private var entityByID = Int2ObjectMaps.emptyMap<Entity>()
+    private var livingBaseByID = Int2ObjectMaps.emptyMap<EntityLivingBase>()
+    private var playersByID = Int2ObjectMaps.emptyMap<EntityPlayer>()
+
     init {
         listener<ConnectionEvent.Disconnect>(Int.MAX_VALUE, true) {
             entity0 = emptyList()
             livingBase0 = emptyList()
             players0 = emptyList()
+
+            entityByID = Int2ObjectMaps.emptyMap()
+            livingBaseByID = Int2ObjectMaps.emptyMap()
+            playersByID = Int2ObjectMaps.emptyMap()
         }
 
         listener<WorldEvent.Entity.Add>(Int.MAX_VALUE, true) {
             entity0 = entity0 + it.entity
+            val entityByID = Int2ObjectOpenHashMap(this.entityByID)
+            entityByID.remove(it.entity.entityId)
+            this.entityByID = entityByID
 
             if (it.entity is EntityLivingBase) {
                 livingBase0 = livingBase0 + it.entity
+                val livingBaseByID = Int2ObjectOpenHashMap(this.livingBaseByID)
+                livingBaseByID.remove(it.entity.entityId)
+                this.livingBaseByID = livingBaseByID
 
                 if (it.entity is EntityPlayer) {
                     players0 = players0 + it.entity
+                    val playersByID = Int2ObjectOpenHashMap(this.playersByID)
+                    playersByID.remove(it.entity.entityId)
+                    this.playersByID = playersByID
                 }
             }
         }
 
         listener<WorldEvent.Entity.Remove>(Int.MAX_VALUE, true) {
             entity0 = entity0 - it.entity
+            val entityByID = Int2ObjectOpenHashMap(this.entityByID)
+            entityByID.remove(it.entity.entityId)
+            this.entityByID = entityByID
 
             if (it.entity is EntityLivingBase) {
                 livingBase0 = livingBase0 - it.entity
+                val livingBaseByID = Int2ObjectOpenHashMap(this.livingBaseByID)
+                livingBaseByID.remove(it.entity.entityId)
+                this.livingBaseByID = livingBaseByID
 
                 if (it.entity is EntityPlayer) {
                     players0 = players0 - it.entity
+                    val playersByID = Int2ObjectOpenHashMap(this.playersByID)
+                    playersByID.remove(it.entity.entityId)
+                    this.playersByID = playersByID
                 }
             }
         }
@@ -59,8 +88,27 @@ object EntityManager : Manager() {
             entity0 = world.loadedEntityList.toList()
             livingBase0 = world.loadedEntityList.filterIsInstance<EntityLivingBase>()
             players0 = world.playerEntities.toList()
+
+            entityByID = mapFromList(entity0)
+            livingBaseByID = mapFromList(livingBase0)
+            playersByID = mapFromList(players0)
         }
     }
+
+    private fun <T : Entity> mapFromList(list: List<T>): Int2ObjectMap<T> {
+        val map = Int2ObjectOpenHashMap<T>(list.size)
+        for (i in list.indices) {
+            val entity = list[i]
+            map[entity.entityId] = entity
+        }
+        return map
+    }
+
+    fun getEntityByID(id: Int): Entity? = entityByID[id]
+
+    fun getLivingBaseByID(id: Int): EntityLivingBase? = livingBaseByID[id]
+
+    fun getPlayerByID(id: Int): EntityPlayer? = playersByID[id]
 
     fun checkEntityCollision(box: AxisAlignedBB, predicate: (Entity) -> Boolean): Boolean {
         return entity.asSequence()
