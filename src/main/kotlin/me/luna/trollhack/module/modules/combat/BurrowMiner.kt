77a431859218f9676eedb3f8361db5ff96361111
Luna <62033805+Luna5ama@users.noreply.github.com>
2023-03-18 18:17:20 -0400

    Corner clip miner

--- src/main/kotlin/me/luna/trollhack/module/modules/combat/BurrowMiner.kt
+++ src/main/kotlin/me/luna/trollhack/module/modules/combat/BurrowMiner.kt
@@ -1,5 +1,6 @@
 package me.luna.trollhack.module.modules.combat
 
+import me.luna.trollhack.event.SafeClientEvent
 import me.luna.trollhack.event.events.TickEvent
 import me.luna.trollhack.event.safeConcurrentListener
 import me.luna.trollhack.manager.managers.CombatManager
@@ -9,6 +10,11 @@ import me.luna.trollhack.module.Module
 import me.luna.trollhack.module.modules.player.PacketMine
 import me.luna.trollhack.util.EntityUtils.betterPosition
 import me.luna.trollhack.util.TickTimer
+import me.luna.trollhack.util.math.vector.distanceSq
+import me.luna.trollhack.util.math.vector.toBlockPos
+import me.luna.trollhack.util.math.xCenter
+import me.luna.trollhack.util.math.zCenter
+import net.minecraft.entity.EntityLivingBase
 import net.minecraft.util.math.BlockPos
 
 @CombatManager.CombatModule
@@ -17,10 +23,15 @@ internal object BurrowMiner : Module(
     category = Category.COMBAT,
     description = "Mines your opponent's burrow"
 ) {
+    private val mode by setting("Mode", Mode.BURROW)
     private val timeout by setting("Timeout", 5000, 0..10000, 100)
 
-    private val timeoutTimer = TickTimer()
     private var lastPos: BlockPos? = null
+    private val timeoutTimer = TickTimer()
+
+    private enum class Mode {
+        BURROW, CORNER
+    }
 
     init {
         onEnable {
@@ -35,21 +46,48 @@ internal object BurrowMiner : Module(
 
         safeConcurrentListener<TickEvent.Post> {
             CombatManager.target?.let { target ->
-                val pos = target.betterPosition
-                if (pos == player.betterPosition) return@safeConcurrentListener
-                val burrow = Burrow.isBurrowed(target)
-                val isHole = HoleManager.getHoleInfo(pos).isHole
-
-                if (burrow || isHole) {
-                    val priority = if (burrow || pos == lastPos) 80 else -100
-                    PacketMine.mineBlock(BurrowMiner, pos, priority)
-                    timeoutTimer.reset()
-                    lastPos = pos
-                } else if (pos != lastPos || timeoutTimer.tick(timeout)) {
-                    reset()
+                when (mode) {
+                    Mode.BURROW -> mineBurrow(target)
+                    Mode.CORNER -> mineCorner(target)
                 }
             } ?: reset()
         }
+    }
+
+    private fun SafeClientEvent.mineBurrow(target: EntityLivingBase) {
+        val pos = target.betterPosition
+        if (pos == player.betterPosition) return
+        val burrow = Burrow.isBurrowed(target)
+        val isHole = HoleManager.getHoleInfo(pos).isHole
+
+        if (burrow || isHole) {
+            val priority = if (burrow || pos == lastPos) 80 else -100
+            PacketMine.mineBlock(BurrowMiner, pos, priority)
+            timeoutTimer.reset()
+            lastPos = pos
+        } else if (pos != lastPos || timeoutTimer.tick(timeout)) {
+            reset()
+        }
+    }
+
+    private fun SafeClientEvent.mineCorner(target: EntityLivingBase) {
+        val pos = getClipPos(target)
+
+        if (pos != null) {
+            PacketMine.mineBlock(BurrowMiner, pos, 80)
+            timeoutTimer.reset()
+            lastPos = pos
+        } else if (timeoutTimer.tick(timeout)) {
+            reset()
+        }
+    }
+
+    private fun SafeClientEvent.getClipPos(target: EntityLivingBase): BlockPos? {
+        val detectBB = target.entityBoundingBox.setMaxY(target.posY + 1.0)
+        val colliedBoxList = world.getCollisionBoxes(null, detectBB)
+        return colliedBoxList.minByOrNull {
+            distanceSq(target.posX, target.posZ, it.xCenter, it.zCenter)
+        }?.center?.toBlockPos()
     }
 
     private fun reset() {
