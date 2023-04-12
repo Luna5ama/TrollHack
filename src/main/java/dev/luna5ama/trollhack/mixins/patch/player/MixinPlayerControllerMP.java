6d74242356958a19506e40b25e04d83cf3d542d4
Luna <62033805+Luna5ama@users.noreply.github.com>
2023-04-12 13:36:19 -0400

    Earth incompatibility fix attempt
    fixes #98

--- src/main/java/dev/luna5ama/trollhack/mixins/patch/player/MixinPlayerControllerMP.java
+++ src/main/java/dev/luna5ama/trollhack/mixins/patch/player/MixinPlayerControllerMP.java
@@ -1,32 +1,16 @@
 package dev.luna5ama.trollhack.mixins.patch.player;
 
 import dev.luna5ama.trollhack.util.Wrapper;
-import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.multiplayer.PlayerControllerMP;
-import net.minecraft.network.play.client.CPacketHeldItemChange;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerControllerMP.class)
 public abstract class MixinPlayerControllerMP {
-    @Shadow
-    private int currentPlayerItem;
-
-    /**
-     * @author Luna
-     * @reason Fuck
-     */
-    @Overwrite
-    private void syncCurrentPlayItem() {
-        EntityPlayerSP player = Wrapper.getPlayer();
-        if (player != null) {
-            int i = player.inventory.currentItem;
-
-            if (i != this.currentPlayerItem) {
-                this.currentPlayerItem = i;
-                player.connection.sendPacket(new CPacketHeldItemChange(this.currentPlayerItem));
-            }
-        }
+    @Inject(method = "syncCurrentPlayItem", at = @At("HEAD"), cancellable = true)
+    private void Inject$syncCurrentPlayItem$HEAD(CallbackInfo ci) {
+        if (Wrapper.getPlayer() == null) ci.cancel();
     }
 }
--- src/main/kotlin/dev/luna5ama/trollhack/util/accessor/Player.kt
+++ src/main/kotlin/dev/luna5ama/trollhack/util/accessor/Player.kt
@@ -1,14 +1,18 @@
 package dev.luna5ama.trollhack.util.accessor
 
+import dev.luna5ama.trollhack.mixins.accessor.player.AccessorPlayerControllerMP
+import dev.luna5ama.trollhack.util.Wrapper
 import net.minecraft.client.multiplayer.PlayerControllerMP
 
 var PlayerControllerMP.blockHitDelay: Int
-    get() = (this as dev.luna5ama.trollhack.mixins.accessor.player.AccessorPlayerControllerMP).blockHitDelay
+    get() = (this as AccessorPlayerControllerMP).blockHitDelay
     set(value) {
-        (this as dev.luna5ama.trollhack.mixins.accessor.player.AccessorPlayerControllerMP).blockHitDelay = value
+        (this as AccessorPlayerControllerMP).blockHitDelay = value
     }
 
-val PlayerControllerMP.currentPlayerItem: Int get() = (this as dev.luna5ama.trollhack.mixins.accessor.player.AccessorPlayerControllerMP).currentPlayerItem
+val PlayerControllerMP.currentPlayerItem: Int get() = (this as AccessorPlayerControllerMP).currentPlayerItem
 
-fun PlayerControllerMP.syncCurrentPlayItem() =
-    (this as dev.luna5ama.trollhack.mixins.accessor.player.AccessorPlayerControllerMP).trollInvokeSyncCurrentPlayItem()
+fun PlayerControllerMP.syncCurrentPlayItem() {
+    if (Wrapper.player == null) return
+    (this as AccessorPlayerControllerMP).trollInvokeSyncCurrentPlayItem()
+}
