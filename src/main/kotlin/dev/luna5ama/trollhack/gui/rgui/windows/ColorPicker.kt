942905f4f55c00f5f98adec626f341bac2e2f92d
Luna <62033805+Luna5ama@users.noreply.github.com>
2023-06-26 20:44:08 -0400

    Added apply button to color picker

--- src/main/kotlin/dev/luna5ama/trollhack/gui/rgui/windows/ColorPicker.kt
+++ src/main/kotlin/dev/luna5ama/trollhack/gui/rgui/windows/ColorPicker.kt
@@ -66,8 +66,9 @@ class ColorPicker(
     // Buttons
     private val buttonOkay = Button(screen, "Okay").action { _, _ -> actionOk() }
     private val buttonCancel = Button(screen, "Cancel").action { _, _ -> actionCancel() }
+    private val buttonApply = Button(screen, "Apply").action { _, _ -> actionApply() }
 
-    private val components = arrayOf(sliderR, sliderG, sliderB, sliderA, buttonOkay, buttonCancel)
+    private val components = arrayOf(sliderR, sliderG, sliderB, sliderA, buttonOkay, buttonCancel, buttonApply)
 
     override fun onDisplayed() {
         r.value = setting.value.r
@@ -79,6 +80,7 @@ class ColorPicker(
         updateHSBFromRGB()
         super.onDisplayed()
         for (component in components) component.onDisplayed()
+        updatePos()
     }
 
     override fun onTick() {
@@ -239,30 +241,30 @@ class ColorPicker(
         val color4 = ColorRGB(0, 255, 255) // 0.5
         val color5 = ColorRGB(0, 0, 255) // 0.6666
         val color6 = ColorRGB(255, 0, 255) // 0.8333
-        val height = (hueLinePos.second.y - hueLinePos.first.y) / 6.0f
+        val partHeight = (huePos.second.y - huePos.first.y) / 6.0f
 
         // Hue slider
         RenderUtils2D.prepareGL()
-        RenderUtils2D.putVertex(hueLinePos.first, color1)
-        RenderUtils2D.putVertex(hueLinePos.first.plus(8.0f, 0.0f), color1)
+        RenderUtils2D.putVertex(huePos.first, color1)
+        RenderUtils2D.putVertex(huePos.first.plus(8.0f, 0.0f), color1)
 
-        RenderUtils2D.putVertex(hueLinePos.first.plus(0.0f, height), color2)
-        RenderUtils2D.putVertex(hueLinePos.first.plus(8.0f, height), color2)
+        RenderUtils2D.putVertex(huePos.first.plus(0.0f, partHeight), color2)
+        RenderUtils2D.putVertex(huePos.first.plus(8.0f, partHeight), color2)
 
-        RenderUtils2D.putVertex(hueLinePos.first.plus(0.0f, height * 2.0f), color3)
-        RenderUtils2D.putVertex(hueLinePos.first.plus(8.0f, height * 2.0f), color3)
+        RenderUtils2D.putVertex(huePos.first.plus(0.0f, partHeight * 2.0f), color3)
+        RenderUtils2D.putVertex(huePos.first.plus(8.0f, partHeight * 2.0f), color3)
 
-        RenderUtils2D.putVertex(hueLinePos.first.plus(0.0f, height * 3.0f), color4)
-        RenderUtils2D.putVertex(hueLinePos.first.plus(8.0f, height * 3.0f), color4)
+        RenderUtils2D.putVertex(huePos.first.plus(0.0f, partHeight * 3.0f), color4)
+        RenderUtils2D.putVertex(huePos.first.plus(8.0f, partHeight * 3.0f), color4)
 
-        RenderUtils2D.putVertex(hueLinePos.first.plus(0.0f, height * 4.0f), color5)
-        RenderUtils2D.putVertex(hueLinePos.first.plus(8.0f, height * 4.0f), color5)
+        RenderUtils2D.putVertex(huePos.first.plus(0.0f, partHeight * 4.0f), color5)
+        RenderUtils2D.putVertex(huePos.first.plus(8.0f, partHeight * 4.0f), color5)
 
-        RenderUtils2D.putVertex(hueLinePos.first.plus(0.0f, height * 5.0f), color6)
-        RenderUtils2D.putVertex(hueLinePos.first.plus(8.0f, height * 5.0f), color6)
+        RenderUtils2D.putVertex(huePos.first.plus(0.0f, partHeight * 5.0f), color6)
+        RenderUtils2D.putVertex(huePos.first.plus(8.0f, partHeight * 5.0f), color6)
 
-        RenderUtils2D.putVertex(hueLinePos.second, color1)
-        RenderUtils2D.putVertex(hueLinePos.second.plus(8.0f, height * 0.0f), color1)
+        RenderUtils2D.putVertex(huePos.first.plus(0.0f, partHeight * 6.0f), color1)
+        RenderUtils2D.putVertex(huePos.first.plus(8.0f, partHeight * 6.0f), color1)
         RenderUtils2D.draw(GL_TRIANGLE_STRIP)
         RenderUtils2D.releaseGL()
 
@@ -270,16 +272,16 @@ class ColorPicker(
         val interpolatedHue = prevHue + (hue - prevHue) * mc.renderPartialTicks
         val pointerPosY = huePos.first.y + fieldHeight * interpolatedHue
         RenderUtils2D.drawTriangleOutline(
-            Vec2f(huePos.first.x - 6.0f, pointerPosY - 2.0f),
-            Vec2f(huePos.first.x - 6.0, pointerPosY + 2.0),
-            Vec2f(huePos.first.x - 2.0f, pointerPosY),
+            Vec2f(huePos.first.x - 5.0f, pointerPosY - 2.0f),
+            Vec2f(huePos.first.x - 5.0f, pointerPosY + 2.0f),
+            Vec2f(huePos.first.x - 1.0f, pointerPosY),
             1.5f,
             GuiSetting.primary
         )
         RenderUtils2D.drawTriangleOutline(
-            Vec2f(huePos.second.x + 2.0f, pointerPosY),
-            Vec2f(huePos.second.x + 6.0, pointerPosY + 2.0),
-            Vec2f(huePos.second.x + 6.0f, pointerPosY - 2.0f),
+            Vec2f(huePos.second.x + 1.0f, pointerPosY),
+            Vec2f(huePos.second.x + 5.0f, pointerPosY + 2.0f),
+            Vec2f(huePos.second.x + 5.0f, pointerPosY - 2.0f),
             1.5f,
             GuiSetting.primary
         )
@@ -324,6 +326,10 @@ class ColorPicker(
         screen.lastClicked = parent
     }
 
+    private fun actionApply() {
+        setting.value = ColorRGB(r.value, g.value, b.value, a.value)
+    }
+
     private fun updateRGBFromHSB() {
         val color = ColorUtils.hsbToRGB(hue, saturation, brightness)
         r.value = color.r
@@ -340,66 +346,67 @@ class ColorPicker(
 
     private fun updatePos() {
         // Red slider
-        sliderR.posY = 4.0f + draggableHeight
-        sliderR.width = 128.0f
+        sliderR.forcePosY = draggableHeight
+        sliderR.forceWidth = 128.0f
 
         // Green slider
-        sliderG.posY = sliderR.posY + sliderR.height + 4.0f
-        sliderG.width = 128.0f
+        sliderG.forcePosY = sliderR.forcePosY + sliderR.forceHeight + 2.0f
+        sliderG.forceWidth = 128.0f
 
         // Blue slider
-        sliderB.posY = sliderG.posY + sliderG.height + 4.0f
-        sliderB.width = 128.0f
+        sliderB.forcePosY = sliderG.forcePosY + sliderG.forceHeight + 2.0f
+        sliderB.forceWidth = 128.0f
 
         // Alpha slider
-        sliderA.posY = sliderB.posY + sliderB.height + 4.0f
-        sliderA.width = 128.0f
+        sliderA.forcePosY = sliderB.forcePosY + sliderB.forceHeight + 2.0f
+        sliderA.forceWidth = 128.0f
 
         // Okay button
-        buttonOkay.posY = sliderA.posY + sliderA.height + 4.0f
-        buttonOkay.width = 50.0f
+        buttonOkay.forcePosY = sliderA.forcePosY + sliderA.forceHeight + 2.0f
+        buttonOkay.forceWidth = 50.0f
 
         // Cancel button
-        buttonCancel.posY = buttonOkay.posY + (buttonOkay.height + 4.0f) * 2.0f
-        buttonCancel.width = 50.0f
+        buttonCancel.forcePosY = buttonOkay.forcePosY + buttonOkay.forceHeight + 2.0f
+        buttonCancel.forceWidth = 50.0f
+
+        // Apply button
+        buttonApply.forcePosY = buttonCancel.forcePosY + buttonCancel.forceHeight + 2.0f
+        buttonApply.forceWidth = 50.0f
 
         // Main window
         dockingH = dev.luna5ama.trollhack.graphics.HAlign.CENTER
         dockingV = dev.luna5ama.trollhack.graphics.VAlign.CENTER
         relativePosX = 0.0f
         relativePosY = 0.0f
-        height = buttonCancel.posY + buttonCancel.height + 4.0f
-        width = height - draggableHeight + 4.0f + 8.0f + 4.0f + 128.0f + 8.0f
+        forceHeight = buttonApply.forcePosY + buttonApply.forceHeight + 4.0f
+        forceWidth = forceHeight - draggableHeight + 4.0f + 8.0f + 4.0f + 128.0f + 8.0f
 
         // PositionX of components
-        sliderR.posX = width - 4.0f - 128.0f
-        sliderG.posX = width - 4.0f - 128.0f
-        sliderB.posX = width - 4.0f - 128.0f
-        sliderA.posX = width - 4.0f - 128.0f
-        buttonOkay.posX = width - 4.0f - 50.0f
-        buttonCancel.posX = width - 4.0f - 50.0f
+        sliderR.forcePosX = forceWidth - 4.0f - 128.0f
+        sliderG.forcePosX = forceWidth - 4.0f - 128.0f
+        sliderB.forcePosX = forceWidth - 4.0f - 128.0f
+        sliderA.forcePosX = forceWidth - 4.0f - 128.0f
+        buttonOkay.forcePosX = forceWidth - 4.0f - 50.0f
+        buttonCancel.forcePosX = buttonOkay.forcePosX
+        buttonApply.forcePosX = buttonOkay.forcePosX
 
         // Variables
-        fieldHeight = height - 8.0f - draggableHeight
+        fieldHeight = forceHeight - draggableHeight - 4.0f
         fieldPos = Pair(
-            Vec2f(4.0f, 4.0f + draggableHeight),
-            Vec2f(4.0f + fieldHeight, 4.0f + fieldHeight + draggableHeight)
+            Vec2f(4.0f, draggableHeight),
+            Vec2f(4.0f + fieldHeight, draggableHeight + fieldHeight)
         )
         huePos = Pair(
-            Vec2f(4.0f + fieldHeight + 8.0f, 4.0f + draggableHeight),
-            Vec2f(4.0f + fieldHeight + 8.0f + 8.0f, 4.0f + fieldHeight + draggableHeight)
-        )
-        hueLinePos = Pair(
-            Vec2f(4.0f + fieldHeight + 8.0f, 4.0f + draggableHeight),
-            Vec2f(4.0f + fieldHeight + 8.0f, 4.0f + fieldHeight + draggableHeight)
+            Vec2f(4.0f + fieldHeight + 6.0f,  draggableHeight),
+            Vec2f(4.0f + fieldHeight + 6.0f + 8.0f, draggableHeight + fieldHeight)
         )
         prevColorPos = Pair(
-            Vec2f(sliderR.posX, buttonOkay.posY),
-            Vec2f(sliderR.posX + 35.0f, buttonCancel.posY - 4.0f)
+            Vec2f(sliderR.forcePosX, buttonOkay.forcePosY),
+            Vec2f(sliderR.forcePosX + 35.0f, forceHeight - 4.0f)
         )
         currentColorPos = Pair(
-            Vec2f(sliderR.posX + 35.0f + 4.0f, buttonOkay.posY),
-            Vec2f(sliderR.posX + 35.0f + 4.0f + 35.0f, buttonCancel.posY - 4.0f)
+            Vec2f(sliderR.forcePosX + 35.0f + 4.0f, buttonOkay.forcePosY),
+            Vec2f(sliderR.forcePosX + 35.0f + 4.0f + 35.0f, forceHeight - 4.0f)
         )
     }
 }
